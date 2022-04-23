5806125b473a2864399468119a2851dfdb4d79ac
philipmasondev <104079365+philipmasondev@users.noreply.github.com>
2022-04-23 19:18:39 -0400

    added model for use with db. Did not add much here but will return to this branch after creting html page

---
+++ pkg/driver/driver.go
@@ -0,0 +1,65 @@
+package driver
+
+import (
+	"database/sql"
+	"log"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+// DB stores connections
+type DB struct {
+	SQL *sql.DB
+}
+
+var dbConn = &DB{}
+
+const maxOpenConn = 10
+const maxIdleConn = 5
+const maxTTL = 5 * time.Minute
+
+// ConnectSQL create db
+func ConnectSQL(dsn string) (*DB, error) {
+	d, err := NewDatabase(dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	d.SetMaxOpenConns(maxOpenConn)
+	d.SetConnMaxIdleTime(maxIdleConn)
+	d.SetConnMaxLifetime(maxTTL)
+
+	dbConn.SQL = d
+
+	err = testConn(d)
+
+	if err != nil {
+		return nil, err
+	}
+	return dbConn, nil
+}
+
+// testConn test db connection by pinging db
+func testConn(d *sql.DB) error {
+	err := d.Ping()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// NewDatabase create new db
+func NewDatabase(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
