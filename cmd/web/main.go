f0939f4ee08325c2f84bb7ea40d824c56a2361eb
Philip M. Mason <104079365+philipmasondev@users.noreply.github.com>
2022-04-23 20:25:51 -0400

    Update main.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,8 +12,7 @@ import (
 const portNumber = ":8080"
 
 // main is the main function
-func main() {
-	var app config.AppConfig
+var app config.AppConfig
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
@@ -23,14 +22,23 @@ func main() {
 	app.TemplateCache = tc
 	app.UseCache = false
 
-	repo := handlers.NewRepo(&app)
-	handlers.NewHandlers(repo)
+	// connect to db
+	log.Println("connecting to db")
+	db, err := driver.ConnectSQL("host=127.0.0.1 port=5432 dbname=postgres user=postgres password=")
+	if err != nil {
+		log.Fatal("Cannot connect to database!")
+	}
+
+	defer db.SQL.Close()
 
 	render.NewTemplates(&app)
 
 	http.HandleFunc("/", handlers.Repo.Home)
-	http.HandleFunc("/search", handlers.Repo.Search)
 
 	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
 }
