104dca81d1b8938904b391bce9d052da47e00094
philipmasondev <104079365+philipmasondev@users.noreply.github.com>
2022-05-05 11:48:11 -0400

    removed print line
    Signed-off-by: philipmasondev <104079365+philipmasondev@users.noreply.github.com>

--- pkg/repository/dbrepo/postgres.go
+++ pkg/repository/dbrepo/postgres.go
@@ -27,8 +27,6 @@ func (m *postgresDBRepo) InsertRecipe(rec string) error {
 
 	json.Unmarshal([]byte(rec), &recipe)
 
-	fmt.Print(recipe)
-
 	sqlStatment := `insert into recipesnonjson (id, meal, drinkalternate, category, area, instructions, mealthumd, tags, youtube, ingredient1, ingredient2, ingredient3, ingredient4,
 		ingredient5, ingredient6, ingredient7, ingredient8, ingredient9, ingredient10, ingredient11, ingredient12, ingredient13, ingredient14, ingredient15, ingredient16,
 		ingredient17, ingredient18, ingredient19, ingredient20, measure1, measure2, measure3, measure4, measure5, measure6, measure7, measure8, measure9, measure10, measure11,
